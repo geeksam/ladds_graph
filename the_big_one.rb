0788ff1714ccb6aa53e1c88114fec09f72c16142
Sam Livingston-Gray <geeksam@gmail.com>
2011-07-15 22:01:06 -0700

    tweak params

--- the_big_one.rb
+++ the_big_one.rb
@@ -6,23 +6,16 @@ require 'ladds_graph'
 
 path = Ladds.path(42)
 
-# path.travel_to 30, 23, 19, 26, 29, 25, 22, 26, 29, 36, 41
-# puts "w00t!" if path.is_circuit?
-# puts path.inspect
-# puts path.unique_streets.inspect
-# puts path.backtracked_streets.inspect
-# puts 'length: %d' % path.length
-# puts 'Score: %d' % path.score
-
 opts = {
-  :max_backtrack_count        => 11,
-  :backtrack_avoidance_factor => 5,
-  :persistence_factor         => 3,
+  :max_backtrack_count        => 7,
+  :backtrack_avoidance_factor => 4,
+  :persistence_factor         => 6,
+  # :randomize                  => true,
 }
 paths = path.go_forth_and_multiply(opts.merge({
   :debug_every_n_steps     => 50_000,
-  # :stop_after_n_iterations => 50_000,
-  :max_path_length         => 200,
+  :stop_after_n_iterations => 50_000,
+  :max_path_length         => 250,
 }))
 
 puts "\n\nDone!"
